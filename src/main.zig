426a2c9df576c4c67527145581602374f27e930c
sea gin <cijin@seagin.me>
2024-11-12 07:31:25 +0700

    hello world server

--- src/main.zig
+++ src/main.zig
@@ -1,28 +1,40 @@
 const std = @import("std");
 const assert = std.debug.assert;
 const http = std.http;
+const net = std.net;
+const print = std.debug.print;
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer assert(gpa.deinit() == .ok);
-    const allocator = gpa.allocator();
+    const addr = net.Address.parseIp4("127.0.0.1", 8080) catch |err| {
+        print("Error occured during ip resolution: {}\n", .{err});
+        return;
+    };
 
-    var client = http.Client{ .allocator = allocator };
-    defer client.deinit();
+    var server = try addr.listen(.{});
+    startServer(&server);
+}
 
-    const uri = std.Uri.parse("https://whatthecommit.com/index.txt") catch unreachable;
+fn startServer(server: *net.Server) void {
+    while (true) {
+        var connection = server.accept() catch |err| {
+            print("Connection interrupted: {}\n", .{err});
+            continue;
+        };
+        defer connection.stream.close();
 
-    var headers = http.Header{ .allocator = allocator };
-    defer headers.deinit();
+        var readBuffer: [8192]u8 = undefined;
+        var httpServer = http.Server.init(connection, &readBuffer);
 
-    try headers.append("accept", "/*");
-    var request = try client.request(.GET, uri, headers, .{});
-    defer request.deinit();
+        var request = httpServer.receiveHead() catch |err| {
+            print("Unable to read head: {}\n", .{err});
+            continue;
+        };
+        handleRequest(&request) catch |err| {
+            print("Unable to handle request: {}\n", .{err});
+        };
+    }
+}
 
-    try request.start();
-    try request.wait();
-    const body = request.reader().readAllAlloc(allocator, 8192) catch unreachable;
-    defer allocator.free(body);
-
-    std.log.info("{s}", .{body});
+fn handleRequest(request: *http.Server.Request) !void {
+    try request.respond("Hello World!\n", .{});
 }
